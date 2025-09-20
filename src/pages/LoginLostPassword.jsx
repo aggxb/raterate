e93dc01919c339fdbdc7ab515dc4b3a5ae036560
aggxb <gabriel241104@icloud.com>
2025-09-20 10:10:41 -0300

    feat: setup initial page routing

---
+++ src/pages/LoginLostPassword.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const LoginLostPassword = () => {
+  return <h1>LoginLostPassword</h1>;
+};
+
+export default LoginLostPassword;
---
+++ src/pages/UserArea.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const UserArea = () => {
+  return <h1>UserArea</h1>;
+};
+
+export default UserArea;
