49173898f03428e2d4586f14b9c95009c2175950
aggxb <gabriel241104@icloud.com>
2025-09-21 12:16:39 -0300

    feat: add shared layout page

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,21 @@ import UserArea from './pages/UserArea';
 import TitleDetails from './pages/TitleDetails';
 import List from './pages/List';
 import NotFound from './pages/NotFound';
+import MainLayout from './pages/MainLayout';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="user/:id" element={<UserArea />} />
+          <Route path="title/:id" element={<TitleDetails />} />
+          <Route path="list/:id" element={<List />} />
+          <Route path="about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
         <Route path="login/*" element={<Login />} />
-        <Route path="user/:id" element={<UserArea />} />
-        <Route path="title/:id" element={<TitleDetails />} />
-        <Route path="list/:id" element={<List />} />
-        <Route path="about" element={<About />} />
-        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
