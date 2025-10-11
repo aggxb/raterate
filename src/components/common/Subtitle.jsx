4297ccc1a98353504b8a80139d1667c94aa346df
Gabriel Augusto <ag71gabriel@gmail.com>
2025-10-11 10:42:21 -0300

    refactor(ui): use styles dictionary in subtitle and paragraph components

--- src/components/common/Subtitle.jsx
+++ src/components/common/Subtitle.jsx
@@ -1,23 +1,7 @@
 import React from 'react';
+import { colors, fonts } from '../../styles/theme';
 
 const Subtitle = ({ children, color, font, Component = 'h2' }) => {
-  const colors = {
-    c1: 'text-c1blue',
-    c2: 'text-c2blue',
-    c3: 'text-c3blue',
-    c4: 'text-c4blue',
-    c5: 'text-c5blue',
-    c6: 'text-c6blue',
-    green: 'text-green',
-    yellow: 'text-yellow',
-    red: 'text-red',
-  };
-
-  const fonts = {
-    subtitle: 'text-h2-l max-md:text-h2-s',
-    subtitleInfo: 'text-h3-m max-md:text-h3-s',
-  };
-
   const fontSize = fonts[font] || fonts.subtitle;
   const textColor = colors[color] || colors.c2;
 
