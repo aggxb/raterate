d777c7456c0814593d058eedd35334ea72a6f6c1
Gabriel Augusto <ag71gabriel@gmail.com>
2025-10-11 11:33:54 -0300

    refactor(ui): adjust dictionary styles definition and usage

--- src/styles/theme.jsx
+++ src/styles/theme.jsx
@@ -1,4 +1,4 @@
-export const colors = {
+export const textColors = {
   c1: 'text-c1blue',
   c2: 'text-c2blue',
   c3: 'text-c3blue',
@@ -10,9 +10,14 @@ export const colors = {
   red: 'text-red',
 };
 
-export const fonts = {
-  subtitle: 'text-h2-l max-md:text-h2-s',
-  subtitleInfo: 'text-h3-m max-md:text-h3-s',
-  paragraph: 'text-p-m max-md:text-p-s',
-  buttonText: 'text-btn-m max-md:text-btn-s',
-};
+export const bgColors = {
+  c1: 'bg-c1blue',
+  c2: 'bg-c2blue',
+  c3: 'bg-c3blue',
+  c4: 'bg-c4blue',
+  c5: 'bg-c5blue',
+  c6: 'bg-c6blue',
+  green: 'bg-green',
+  yellow: 'bg-yellow',
+  red: 'bg-red',
+}
